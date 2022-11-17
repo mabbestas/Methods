59f102a778a6471f0d611d3e90953980ba0a1d10
Burak Bestas <mabbestas@gmail.com>
2022-11-17 18:37:28 +0300

    Methods added

---
+++ Methods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a + b);
+
+            int sonuc = Topla(2, 3);
+            Console.WriteLine(sonuc);
+
+            static int Topla(int deger1, int deger2)
+            {
+                return (deger1 + deger2);
+            }
+
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+
+            int sonuc2 = ornek.ArttırVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(a + 6));
+            ornek.EkranaYazdir(Convert.ToString(a + 6)); 
+        }
+
+        class Metotlar
+        {
+            public void EkranaYazdir(string veri)
+            {
+                Console.WriteLine(veri);
+            }
+
+
+            public int ArttırVeTopla(ref int deger1, ref int deger2)
+            {
+                deger1 += 1;
+                deger2 += 1;
+                return deger1 + deger2;
+            }
+        }
+    }
+}
